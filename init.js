ba6539f2724055340c8577b32a0f012c2076ac74
lauer3912 <lauer3912@gmail.com>
2024-04-30 09:10:31 +0800

    init

--- init.js
+++ init.js
@@ -8,9 +8,9 @@ const fs = require('fs');
 const path = require('path');
 
 // ------------------------------------------------------------------------------------------- 以下内容需要自己替换
-const subSiteAuthor = 'Nova';
-const subSiteKeywords = `` + ','; // 关键字，多个关键字用逗号分隔
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
+const subSiteAuthor = 'Ava';
+const subSiteKeywords = `smart video creator, smart video editor, smart ai video` + ','; // 关键字，多个关键字用逗号分隔
+const indexNowKey = '6c7be27196a0481ba5346d1b68048cba'; // 替换为你的API密钥
 
 
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# TechiDaily
+# Video Content Creator Site
 
-Welcome to [https://android-transfer.techidaily.com](https://android-transfer.techidaily.com)
+Welcome to [https://video-content-creator.techidaily.com](https://video-content-creator.techidaily.com)
 
 ## Publish
 
